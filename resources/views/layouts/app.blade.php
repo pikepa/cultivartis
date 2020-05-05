c991bc174c5d1bfcf1cc8227501576e852dfd7df
Peter Pike <pikepeter@gmail.com>
2020-05-05 17:35:09 +0800

    added layout to welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,21 +22,21 @@
         <nav class="bg-yellow-600 shadow mb-8 py-6">
             <div class="container mx-auto px-6 md:px-0">
                 <div class="flex items-center justify-center">
-                    <div class="mr-6">
-                        <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
+                    <div class=" mr-6">
+                        <a href="{{ url('/') }}" class="text-gray-800 text-lg font-semibold text-gray-100 no-underline">
                             {{ config('app.name', 'Laravel') }}
                         </a>
                     </div>
                     <div class="flex-1 text-right">
                         @guest
-                        <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="text-gray-800 no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
                         {{-- @if (Route::has('register'))
                                 <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('register') }}">{{ __('Register') }}</a>
                         @endif --}}
                         @else
-                        <span class="text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
+                        <span class="text-gray-800 text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
 
-                        <a href="{{ route('logout') }}" class="no-underline hover:underline text-gray-300 text-sm p-3" onclick="event.preventDefault();
+                        <a href="{{ route('logout') }}" class="text-gray-800 no-underline hover:underline text-gray-300 text-sm p-3" onclick="event.preventDefault();
                                     document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
                             {{ csrf_field() }}
