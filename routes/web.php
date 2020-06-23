a1f2851cb5a8b5ea0be026a49d73f9d2be75a311
Peter Pike <pikepeter@gmail.com>
2020-06-23 15:02:35 +0800

    disable route register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'HomeController@welcome')->name('welcome');
