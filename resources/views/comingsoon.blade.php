7d8f94e2683783bbf19a63445a9ed419ff2c6b87
Peter Pike <pikepeter@gmail.com>
2020-06-26 19:57:47 +0800

    trying to fix

---
+++ resources/views/comingsoon.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="flex flex-col items-center ">
+    <div>
+        <H1 class="text-pink text-6xl my-8">Sorry this page is coming Soon</H1>   
+        </div>
+ 
+</div>
+@endsection
