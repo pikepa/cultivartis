c7ae161b05e7599ce3eaae245652f0a98ffa77ed
Peter Pike <pikepeter@gmail.com>
2020-12-11 06:28:19 +0800

    added horizon no special config

--- resources/views/emails/confirmcontact.blade.php
+++ resources/views/emails/confirmcontact.blade.php
@@ -1,9 +1,13 @@
-<p>Hello {{$contact->firstname}}
-</p>
-<p>You are receiving this email because you registered to receive information about our book "The Erlang Enigma".</p>
-<p>To confirm you wish to receive futher updates from us at this email address ({{$contact->email}}), please click on the link below</p>
-<button>Confirm</button>
-<p>If this was not you, then please ignore this email and we will not bother you again.</p>
+<div class="m-4 text-xl">
+    <p class='mb-2'>Hello {{$contact->firstname}}
+    </p>
+    <p class='mb-2'>You are receiving this email because you registered to receive information about our book <strong>"The Erlang Enigma"</strong>.</p>
+    <p class='mb-2'>To confirm you wish to receive futher updates from us at this email address ({{$contact->email}}), please click on the link below.</p>
+    <div class="mb-2 text-2xl font-semibold text-center">
+        <a href="#">Click here to confirm your subscription.</a>
+    </div>
+    <p class='mb-2'>If this was not you, then please ignore this email and we will not bother you again.</p>
 
-<p>Many thanks for your time </p>
-<p>Ray and Bruce</p>
+    <p class='mb-2'>Many thanks for your time. </p>
+    <p class='mb-2'>Ray and Bruce</p>
+</div>
