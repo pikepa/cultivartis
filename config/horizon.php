932dc45076ec472925e6bdaf8ff03d9a2b2e9bac
Peter Pike <pikepeter@gmail.com>
2020-12-11 11:14:03 +0800

    Git Hub Config Changes
    Added pikepeter@gmail.com as allowed user
    Set  email then default queues

--- config/horizon.php
+++ config/horizon.php
@@ -167,7 +167,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default','email'],
+            'queue' => ['email','default'],
             'balance' => 'auto',
             'maxProcesses' => 1,
             'memory' => 128,
