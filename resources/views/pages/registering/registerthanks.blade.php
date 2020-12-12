85158a6af16784e4e85e71e1f6c5898dd803af99
Peter Pike <pikepeter@gmail.com>
2020-12-12 11:24:28 +0800

    Update to thanks for Registering

--- resources/views/pages/registering/registerthanks.blade.php
+++ resources/views/pages/registering/registerthanks.blade.php
@@ -1,7 +1,9 @@
 <x-layouts.app>
-    <div>
-        <h1>Thank you for registering</h1>
-        <p>
-            You will receive an email shortly which we need you to confirm your registration</p>
+    <div class="p-8 text-center">
+        <h1 class="mb-8 text-4xl">Thank you for registering</h1>
+        <p class="text-2xl">
+            You will receive an email shortly with a special link to confirm your registration.</p>
+        <a href="/">
+            <button class="p-3 mt-4 text-gray-800 rounded-lg bg-gold ">Goto Home page</button></a>
     </div>
 </x-layouts.app>
