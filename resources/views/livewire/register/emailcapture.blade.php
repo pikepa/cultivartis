21a0f64893cb07c114bf809f205ae59f54312c47
Peter Pike <pikepeter@gmail.com>
2020-12-18 16:21:33 +0800

    Update emailcapture.blade.php
    Slight adjustment to Email capture view, inlude light gray background rounded

--- resources/views/livewire/register/emailcapture.blade.php
+++ resources/views/livewire/register/emailcapture.blade.php
@@ -1,10 +1,10 @@
-<div class="mt-4">
+<div class="p-6 mt-4 rounded bg-gray-50">
     <form wire:submit.prevent="register">
         <div>
             <label for="firstname" class="block text-sm font-medium leading-5 text-gray-700">First Name
             </label>
             <div class="mt-2">
-                <input type=text wire:model="firstname" id="firstname" name="firstname" class="block w-full pl-3 rounded-md shadow-sm form-input sm:text-sm sm:leading-5" placeholder="Tom Jones" />
+                <input type=text wire:model="firstname" id="firstname" name="firstname" class="block w-full pl-3 rounded-md shadow-sm form-input sm:text-sm sm:leading-5" placeholder="Tom" />
                 <div class="mt-2 text-red-600">
                     @error('firstname') <span>{{ $message }}</span> @enderror
                 </div>
@@ -15,7 +15,7 @@
             </label>
             <div class="mt-2 ">
                 <div>
-                    <input type=text wire:model="familyname" id="familyname" name="familyname" class="block w-full pl-3 rounded-md shadow-sm form-input sm:text-sm sm:leading-5" placeholder="Tom Jones" />
+                    <input type=text wire:model="familyname" id="familyname" name="familyname" class="block w-full pl-3 rounded-md shadow-sm form-input sm:text-sm sm:leading-5" placeholder="Jones" />
                 </div>
                 <div class="mt-2 text-red-600">
                     @error('familyname') <span>{{ $message }}</span> @enderror
@@ -56,7 +56,7 @@
         </div>
         <div class="mt-6">
             <span class="rounded-md shadow-sm ">
-                <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 border border-transparent rounded-md bg-gold hover:bg-yellow-500 focus:outline-none focus:border-indigo-700">
+                <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 border border-transparent rounded-md bg-gold hover:bg-yellow-200 focus:outline-none focus:border-indigo-700">
                     Register your Interest
                 </button>
             </span>
