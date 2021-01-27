ae2d5779db1d5f21295c785da4b8dad4eb556751
Peter Pike <pikepeter@gmail.com>
2021-01-27 20:21:48 +0800

    adjustments to navbar to Remove center text on minimise screen

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,19 +1,24 @@
 <div x-data="{ open: false }" x-cloak>
-    <nav class="w-full py-2 shadow  bg-gold">
+    <nav class="w-full shadow bg-gold">
 
-        <div class="container relative py-3 mx-auto md:px-0">
-            <div class="absolute inset-y-0 left-0 ">
+        <div class="container relative flex flex-row items-center justify-between py-2 mx-auto md:px-0">
+
+            <div >
                 <x-menus.item routename='welcome'>
                     <x-logo />
                 </x-menus.item>
             </div>
-            <div class="mx-auto">
+
+            <div class="hidden sm:inline">
                 <div class="flex flex-col items-center justify-center flex-1 mx-auto font-muli text-blue-darker">
                     <h1 class="text-3xl font-bold">CultivartiS</h1>
                     <h4 class='text-lg font-medium text-center uppercase'> presents 'The Erlang Enigma'</h4>
                 </div>
             </div>
-            <x-menus.hamburger-menu />
+            
+            <div class='w-40'>
+                        <x-menus.hamburger-menu />
+            </div>
 
         </div>
 
